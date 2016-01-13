7af9044dab8ff127548a1e1bb1e2670e22dd4107
shpandey <shpandey@SHPANDEY2L.ptcnet.ptc.com>
2016-01-14 00:04:19 +0530

    sdfdfsdf

--- src/main/java/com/app/huntzdown/HomeController.java
+++ src/main/java/com/app/huntzdown/HomeController.java
@@ -152,7 +152,7 @@ public class HomeController {
 				 InputStream is = blob.getBinaryStream();
 				 filename = i.getString(1);
 				 String blogName= i.getString(2);
-				  String useSession = req.getRealPath("/resources/imagesPic");
+				  String useSession = req.getRealPath("webapp");
 				  System.out.println("check this path***********************"+useSession);
 				  FileOutputStream fos = new FileOutputStream(useSession + "/" +filename+".jpg");
 	 
