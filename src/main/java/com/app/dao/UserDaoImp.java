e80c538cbb403f205b3e1fe4742fbe0098e4152a
shpandey <shpandey@SHPANDEY2L.ptcnet.ptc.com>
2017-08-16 21:12:10 +0530

    lknklnkl

--- src/main/java/com/app/dao/UserDaoImp.java
+++ src/main/java/com/app/dao/UserDaoImp.java
@@ -846,7 +846,7 @@ System.out.println("inside product list..");
 
 	@Override
 	public void addWifiData(String name, String surname) {
-		String sql = "INSERT INTO huntzdown.wifiData(name,wifiData) VALUES(?,?)";
+		String sql = "INSERT INTO huntzdown.wifiData(name,surname) VALUES(?,?)";
 		jdbcTemplate.update(sql, new Object[] { name,"title"});
 		
 	}
